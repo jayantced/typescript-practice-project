a32a557d7a52d496eeecd48d41061346926d3582
jayantced <jayant@cedcommerce.com>
2024-01-22 17:50:27 +0530

    ss

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -1,33 +1,48 @@
-import { ComponentPropsWithoutRef, ReactNode } from "react"
+import { ComponentPropsWithoutRef, ReactNode } from "react";
 import { Link, type LinkProps } from "react-router-dom";
 
 type BaseProps = {
-    children: ReactNode;
-    textOnly: boolean;
-}
+  children: ReactNode;
+  textOnly: boolean;
+};
 
-type ButtonProps = ComponentPropsWithoutRef<'button'> & BaseProps & {
+type ButtonProps = ComponentPropsWithoutRef<"button"> &
+  BaseProps & {
     to?: never;
-}
+  };
 
-type ButtonLinkProps = LinkProps & BaseProps & {
+type ButtonLinkProps = LinkProps &
+  BaseProps & {
     to: string;
-}
+  };
 
-function isRouterLink(props: ButtonProps | ButtonLinkProps): props is ButtonLinkProps {
-    return 'to' in props;
+function isRouterLink(
+  props: ButtonProps | ButtonLinkProps
+): props is ButtonLinkProps {
+  return "to" in props;
 }
 
 export default function Button(props: ButtonProps | ButtonLinkProps) {
-    if (isRouterLink(props)) {
-        const { children, textOnly, ...otherProps } = props
-        return <Link className={`button ${textOnly ? 'button--text-only' : ''}`} {...otherProps}>{children}</Link>
-    }
+  if (isRouterLink(props)) {
+    const { children, textOnly, ...otherProps } = props;
+    return (
+      <Link
+        className={`button ${textOnly ? "button--text-only" : ""}`}
+        {...otherProps}
+      >
+        {children}
+      </Link>
+    );
+  }
 
-    const { children, textOnly, ...otherProps } = props
+  const { children, textOnly, ...otherProps } = props;
 
   return (
-    <button className={`button ${textOnly ? 'button--text-only' : ''}`}
-    {...otherProps}>{children}</button>
-  )
+    <button
+      className={`button ${textOnly ? "button--text-only" : ""}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
 }
